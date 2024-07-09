b84d9fd6245eb321f3edafff99d9ff7add8b44b1
Codingbot97 <132872429+Codingbot97@users.noreply.github.com>
2024-07-09 12:55:45 +0530

    first

---
+++ config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://Student:0alce7ZPKMTARkAQ@cluster0.nyl0yjx.mongodb.net/Bools-Collections?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
